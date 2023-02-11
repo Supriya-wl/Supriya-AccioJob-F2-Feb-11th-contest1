8ea2cb71e494556f6d3e84166ad248698c68e275
Supriya Reddy <121212953+Supriya-wl@users.noreply.github.com>
2023-02-11 23:19:28 +0530

    committed

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ let arr = [
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
+let arr1 = [
+  { id: 4, name: "Supriya", age: "28", marks: 90 },
+  { id: 5, name: "Sushma", age: "20", marks: 85 },
+  { id: 6, name: "Srujana", age: "19", marks: 45 },
+];
+
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  //console.log(arr.map)
+  console.log(arr.filter(stu => stu.marks > 50).map(stu => stu));
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  //arr.forEach(stud => stud.marks > 50 (console.log(stud.name)));
+  //console.log(arr.forEach(stu => stu.marks > 50));
+  //console.log(arr.forEach(number => number.marks > 50);(arr.forEach(number => number.marks > 50) console.log(number));
+ 
+  
 }
 
 function addData() {
@@ -20,8 +33,11 @@ function addData() {
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  console.log(arr.marks<50);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+
+  console.log(arr.concat(arr1));
 }
